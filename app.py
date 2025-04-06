df798f660aa3ad6bcef45f114619ab8b995813ed
YashaswiniPanigrahy <yashaswinipanigrahy@gmail.com>
2025-04-06 15:02:49 +0530

    Updated Flask route in app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return "hello World"
 
+@app.route('/<name>')
+def user(name):
+    return f"Hello {name}"
+
 if __name__ == "__main__":
     app.run(debug=True)
